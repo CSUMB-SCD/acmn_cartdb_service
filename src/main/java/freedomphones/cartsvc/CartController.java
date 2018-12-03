2dc8bedd9ecd45e15b5a5f9b5a2f854463992500
cforsythe <cforsythe@csumb.edu>
2018-12-02 22:05:28 -0800

    cant figure this out

--- src/main/java/freedomphones/cartsvc/CartController.java
+++ src/main/java/freedomphones/cartsvc/CartController.java
@@ -2,7 +2,6 @@ package freedomphones.cartsvc;
 
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +21,7 @@ public class CartController{
     @Autowired
     ICartRepository cartRepository;
 
-    @PostMapping(value = "/addItem/{username}", consumes="application/json")
+    @PostMapping(value = "/addItem/{username}")
     @ResponseBody
     public String addItem(@RequestBody Item item, @PathVariable String username) {
         Cart cart = cartRepository.findByUsername(username);
